2bfaa9e322645cc8233671479cb46183c792d32a
Mohit Kumar <mohitkumar@stock12959.local>
2018-11-30 10:14:56 +0530

    Initial Commit

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Mohit Kumar on 30/11/18.
+//  Copyright © 2018 Mohit Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
